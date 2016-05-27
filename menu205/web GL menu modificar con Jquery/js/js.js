55b1ff197215dfcbdd99f8ddb3456975a781bcf1
escuelita <escuelita>
2016-05-27 10:21:05 -0300

    Ejercicio menu 20-5

---
+++ menu205/web GL menu modificar con Jquery/js/js.js
@@ -0,0 +1,23 @@
+$(function(){
+
+$("#pri").css({'background-color':'blue',"color":"yellow"});
+$(".seg").css({'background-color':'white',"color":"red"});
+//$(".lista li:eq(2)").remove(); // tercer elemento li
+$("li:eq(2)").remove();
+
+//funciones de jQuery
+//.css
+//.remove()
+//setea un evento
+//.om("evento",)
+//.attr
+//agregar a un ul un elemento
+//$("ul").append(elemnto)
+//agregar un elemnto a un ul
+//elmento.appendTo(ul)
+//
+
+
+});
+
+
